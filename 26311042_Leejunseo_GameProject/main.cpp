338a68d9365f7db7696543fec30b3ce6732c3d17
itachi08160413 <itachi08160413@gmail.com>
2026-09-02 17:53:15 +0900

    nuget

--- 26311042_Leejunseo_GameProject/main.cpp
+++ 26311042_Leejunseo_GameProject/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include "glc2d.h"
+#include <stdio.h>
 
-int main(void)
+int main()
 {
-	std::cout << "My Game Project \n";
+    // SDK 초기화
+    g2_InitSdk();
+
+    printf("Starting ...\n\n");
+
+    // 윈도우 생성
+    g2_CreateWin(100, 100, 1024, 600, "My First Game Window");
+
+    // 실행
+    g2_Run();
+
+    // 윈도우 해제
+    g2_DestroyWin();
+
+    return 0;
 }
